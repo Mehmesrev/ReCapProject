b42a75313d407ff0f76193acbbc4a25dfbf7c9bf
Mehmesrev <mehmesrev@outlook.com>
2024-07-04 09:46:13 +0300

    add and delete tests

--- Console/Console/Program.cs
+++ Console/Console/Program.cs
@@ -11,10 +11,55 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new EfCarDal());
-            carManager.Add();
-            
+            //DeleteCarTest();
+            //DeleteBrandTest();
+            //AddBrandTest();
+            //AddCarTest();
+
         }
-    
+
+        private static void AddCarTest()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            Car newCar = new Car()
+            {
+                BrandId = 4,
+                ColorId = 1,
+                ModelYear = 2024,
+                DailyPrice = 1000,
+                Description = "Wir produzieren Technologie, andere wenden sie an"
+            };
+            carManager.Add(newCar);
+        }
+
+        private static void AddBrandTest()
+        {
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            Brand newBrand = new Brand()
+            {
+                Name = "Mesrewagen"
+            };
+            brandManager.Add(newBrand);
+        }
+
+        private static void DeleteBrandTest()
+        {
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            Brand oldBrand = new Brand()
+            {
+                Id = 1
+            };
+            brandManager.Delete(oldBrand);
+        }
+
+        static void DeleteCarTest()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            Car oldCar = new Car
+            {
+                Id = 7
+            };
+            carManager.Delete(oldCar);
+        }
     }
 }
